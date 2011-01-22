51509231db7cc08b635b8c8bfde5b384f32bfd70
fabian.lindenberg <fabian.lindenberg@05781afb-0dcb-75a7-b6e7-0f9d01be9952>
2011-01-22 02:23:13 +0000

    * check for content articles * token stemming

--- trunk/src/ir/assignment03/WikiProcessor.java
+++ trunk/src/ir/assignment03/WikiProcessor.java
@@ -1,5 +1,7 @@
 package ir.assignment03;
 
+import java.util.StringTokenizer;
+
 /**
  * CS 121 Information Retrieval
  * Assignment 03
@@ -12,14 +14,98 @@ package ir.assignment03;
 public class WikiProcessor {
 
 	/**
+	 * The relative path of all Wikipedia articles 
+	 */
+	private static final String DEFAULT_PATH = "wiki/";
+	/**
+	 * Prefixes of all pages other than content articles
+	 */
+	private static final String DIFFERENT_TYPES = "(" +
+													"(Category)" +
+													"|(Category_talk)" +
+													"|(File)" +
+													"|(Image_talk)" +
+													"|(Help)" +
+													"|(Media)" +
+													"|(MediaWiki)" +
+													"|(MediaWiki_talk)" +
+													"|(Portal)" +
+													"|(Special)" +
+													"|(Talk)" +
+													"|(Template)" +
+													"|(Template_talk)" +
+													"|(User)" +
+													"|(User_talk)" +
+													"|(Wikipedia)" +
+													"|(Wikipedia_talk)" +
+													"):.*";
+	/**
+	 * Delimiters used for tokenizing the content
+	 */
+	private static final String TOKEN_DELIMITER = " \t\n\r\f\".,:;!?'~@#*+§$%&()=`´{[]}|<>";
+	
+	/**
+	 * The full path leading to articles (combination of host and relative path)
+	 */
+	private String articlePath;
+	/**
+	 * The Porter Stemmer used to stem tokens
+	 */
+	private Stemmer stemmer;
+
+	/**
+	 * Initiates an instance of {@link WikiProcessor}.
+	 * 
+	 * @param hostname the name of host 
+	 */
+	public WikiProcessor(String hostname) {
+		if (!hostname.endsWith("/")) {
+			hostname += "/";
+		}
+		this.articlePath = hostname + DEFAULT_PATH;
+		this.stemmer = new Stemmer();
+	}
+
+	/**
 	 * Process the passed <code>content</code> of the web page with the URL <code>url</code>.
 	 * 
 	 * @param url the URL of the web page
 	 * @param content the content of the web page
 	 */
 	public void process(String url, String content) {
-		// TODO Auto-generated method stub
-		System.out.println("processed: " + url);
+		String title = extractTitle(url);
+		if (title != null && isContentPage(title)) {
+			StringTokenizer tokenizer = new StringTokenizer(content,TOKEN_DELIMITER); // tokenize content
+			while (tokenizer.hasMoreTokens()) { // stem tokens
+				char[] token = tokenizer.nextToken().toCharArray();
+				this.stemmer.add(token,token.length);
+				this.stemmer.stem();
+//				System.out.println(String.valueOf(token) + " - " + this.stemmer.toString());
+			}
+		}
+	}
+
+	/**
+	 * Extracts the title of the web page from the <code>url</code>
+	 * 
+	 * @param url the URL of the web page
+	 * @return the title of the web page or <code>null</code> if no title could be extracted
+	 */
+	private String extractTitle(String url) {
+		if (url.startsWith(articlePath)) {
+			return url.substring(articlePath.length());
+		}
+		return null;
+	}
+
+	/**
+	 * Determines whether the article is a content article based on the structure of its title.  
+	 * 
+	 * @param title the title of the article
+	 * @return true if the article is a content article, false otherwise
+	 */
+	private boolean isContentPage(String title) {
+		return !title.matches(DIFFERENT_TYPES);
 	}
 
 	/**
