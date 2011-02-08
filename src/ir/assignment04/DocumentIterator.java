9455148f08054c5f56bc925d41ab2c598f8b8531
fabian.lindenberg <fabian.lindenberg@05781afb-0dcb-75a7-b6e7-0f9d01be9952>
2011-02-08 17:38:21 +0000

    * implemented lazy initialization for document iterators

--- src/ir/assignment04/DocumentIterator.java
+++ src/ir/assignment04/DocumentIterator.java
@@ -3,7 +3,6 @@ package ir.assignment04;
 import java.io.File;
 import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
@@ -21,10 +20,26 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
  */
 public class DocumentIterator implements Iterator<File> {
 
+	/**
+	 * The root directory
+	 */
 	private File root;
+	/**
+	 * The cached next file
+	 */
 	private File next;
+	/**
+	 * All files in the root directory
+	 */
 	private Queue<File> files;
-	private List<DocumentIterator> iterators;
+	/**
+	 * All subfolders in the root directory
+	 */
+	private Queue<File> directories;
+	/**
+	 * The iterator for the currently chosen subfolder
+	 */
+	private DocumentIterator subfolderIterator;
 	
 	public DocumentIterator(String root) {
 		this(new File(root));
@@ -35,19 +50,18 @@ public class DocumentIterator implements Iterator<File> {
 		if(!this.root.isDirectory()) {
 			throw new IllegalArgumentException("The path provided is not a directory.");
 		}
-		// TODO: implement lazy initialization
 		initialize();
 		next();
 	}
 	
 	private void initialize() {
-		this.iterators = new LinkedList<DocumentIterator>();
+		this.directories = new LinkedList<File>();
 		this.files = new LinkedList<File>();
 		
 		File[] files = this.root.listFiles();
 		for (File f : files) {
 			if (f.isDirectory()) {
-				this.iterators.add(new DocumentIterator(f));
+				this.directories.add(f);
 			} else {
 				this.files.add(f);
 			}
@@ -63,21 +77,40 @@ public class DocumentIterator implements Iterator<File> {
 	public File next() {
 		// cache the element after the next
 		File cache = null;
-		if (!this.files.isEmpty()) {
+		if (!this.files.isEmpty()) { // iterate over own files first
 			cache = this.files.poll();
-		} else {
-			for (DocumentIterator it : this.iterators) {
-				if (it.hasNext()) {
-					cache = it.next();
-					break;
-				}
-			}
+		} else { // if no files remain, utilize iterator for subfolders
+			chooseNewIteratorIfNecessary();
+			if (this.subfolderIterator != null && this.subfolderIterator.hasNext()) {
+				cache = this.subfolderIterator.next();
+			} 
 		}
 		
 		// return the next element
 		File result = this.next;
 		this.next = cache;
 		return result;
+	}
+
+	/**
+	 * Chooses new iterator if none is initialized or if the current one does not offer a next element.
+	 * 
+	 * If no appropriate iterator can be found, the subfolderIterator is set to null.
+	 */
+	private void chooseNewIteratorIfNecessary() {
+		boolean success = false;
+		if (this.subfolderIterator == null || !this.subfolderIterator.hasNext()) {
+			while (!this.directories.isEmpty()) {
+				this.subfolderIterator = new DocumentIterator(this.directories.poll());
+				if (this.subfolderIterator.hasNext()) {
+					success = true;
+					break;
+				}
+			}
+		}
+		if (!success) {
+			this.subfolderIterator = null;
+		}
 	}
 
 	@Override
