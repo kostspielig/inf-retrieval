c523105f29b87791a7cd6db313fcc8fa4f130c75
leavael <leavael@05781afb-0dcb-75a7-b6e7-0f9d01be9952>
2011-02-18 20:37:20 +0000

    made package for assignment05 and added wordcount in book to (new) quantifier class

---
+++ trunk/src/ir/assignment05/Quantifier.java
@@ -0,0 +1,41 @@
+package ir.assignment05;
+
+import java.util.Vector;
+
+import ir.assignment02.IO;
+import ir.assignment04.Parser;
+
+
+/**
+ * CS 121 Information Retrieval <br />
+ * Assignment 05 <br />
+ * Part 2 <br /><br />
+ * 
+ * @author María Carrasco
+ * @author Fabian Lindenberg
+ * @author Lea Voget
+ *
+ */
+public class Quantifier {
+
+	public static void main(String[] args) {
+		if (args.length < 1) {
+			System.out.println("Please specify the file where the book is stored.");
+			return;
+		}
+		String book = IO.getFileContent(args[0]);
+		int totalWords = tokenizeWords(book).size();
+		System.out.println("Total words of " + args[0] + " is " + totalWords);
+		
+		// built index
+		int occurencesOfLove;
+		int occurencesOfAdventure;
+		String[] topWords = new String[5];
+	}
+
+	private static Vector<String> tokenizeWords(String book) {
+		Parser parser = new Parser();
+		Vector<String> tokens = parser.parse(book);
+		return tokens;
+	}
+}
