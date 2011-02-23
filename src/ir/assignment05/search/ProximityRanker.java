86d5b17430f79d2b50ae89c2409fca489023bc0e
kostspielig <kostspielig@05781afb-0dcb-75a7-b6e7-0f9d01be9952>
2011-02-23 21:03:39 +0000

    set up for the search interface

---
+++ src/ir/assignment05/search/ProximityRanker.java
@@ -0,0 +1,14 @@
+package ir.assignment05.search;
+
+/**
+ * CS 121 Information Retrieval <br />
+ * Assignment 05 <br />
+ * Part 3 <br /><br />
+ * 
+ * @author María Carrasco
+ * @author Fabian Lindenberg
+ * @author Lea Voget
+ */
+public class ProximityRanker implements RankingMethod {
+
+}
---
+++ src/ir/assignment05/search/Query.java
@@ -0,0 +1,13 @@
+package ir.assignment05.search;
+
+public class Query {
+
+	/**
+	 * the query string
+	 */
+	private String q;
+	
+	public Query(String q) {
+		this.q = q;
+	}
+}
