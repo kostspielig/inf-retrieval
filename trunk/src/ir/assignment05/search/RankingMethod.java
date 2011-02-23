4697180bd75559e2847b3b4b662a5d5dabf2d283
kostspielig <kostspielig@05781afb-0dcb-75a7-b6e7-0f9d01be9952>
2011-02-23 21:03:39 +0000

    set up for the search interface

---
+++ trunk/src/ir/assignment05/search/RankingMethod.java
@@ -0,0 +1,17 @@
+package ir.assignment05.search;
+
+/**
+ * CS 121 Information Retrieval <br />
+ * Assignment 05 <br />
+ * Part 3 <br /><br />
+ * 
+ * Interface common to all ranking methods that can be passed to the {@link Search} interface
+ * 
+ * @author María Carrasco
+ * @author Fabian Lindenberg
+ * @author Lea Voget
+ *
+ */
+public interface RankingMethod {
+
+}
---
+++ trunk/src/ir/assignment05/search/Search.java
@@ -0,0 +1,23 @@
+package ir.assignment05.search;
+
+/**
+ * CS 121 Information Retrieval <br />
+ * Assignment 05 <br />
+ * Part 3 <br /><br />
+ * 
+ * @author María Carrasco
+ * @author Fabian Lindenberg
+ * @author Lea Voget
+ */
+public class Search {
+
+	private static final RankingMethod DEFAULT_METHOD = new TfIdfRanker();
+
+	public void search(Query q) {
+		search(q, DEFAULT_METHOD);
+	}
+	
+	public void search(Query q, RankingMethod method) {
+		// TODO: implement this :-)
+	}
+}
