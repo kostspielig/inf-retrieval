900c0d479996858e3a6f4620b7b518db7d92bd3e
fabian.lindenberg <fabian.lindenberg@05781afb-0dcb-75a7-b6e7-0f9d01be9952>
2011-02-26 02:10:52 +0000

    * fixed ranking order bug * cosinesimranker implemented * (stupid but mindfucking complex) proximity ranker

--- trunk/src/ir/assignment05/search/Search.java
+++ trunk/src/ir/assignment05/search/Search.java
@@ -1,5 +1,7 @@
 package ir.assignment05.search;
 
+import java.util.PriorityQueue;
+
 /**
  * CS 121 Information Retrieval <br />
  * Assignment 05 <br />
@@ -11,18 +13,36 @@ package ir.assignment05.search;
  */
 public class Search {
 	
-	public static Iterable<SearchResult> search(Query q, RankingMethod method) {
+	public static PriorityQueue<SearchResult> search(Query q, RankingMethod method) {
 		return method.search(q);
 	}
 	
 	
 	public static void main(String[] args) {
-		RankingMethod m = new TfIdfRanker(args[0]);
+		RankingMethod m1 = new TfIdfRanker(args[0]);
+		RankingMethod m2 = new CosineSimRanker(args[0]); // TODO: order is inversed
+		RankingMethod m3 = new ProximityRanker(args[0]);
 		QueryBuilder qb = new QueryBuilder ();
+		Query q = qb.construct("anna karenina");
 		
-		Iterable<SearchResult> r = Search.search(qb.construct("maria"), m);
+		System.out.println("tfidf");
+		PriorityQueue<SearchResult> r1 = Search.search(q, m1);
+		SearchResult res;
+		while ((res = r1.poll()) != null)
+			System.out.println(res.toString());
 		
-		for (SearchResult res : r)
+		System.out.println("#######################");
+		
+		System.out.println("cosine");
+		PriorityQueue<SearchResult> r2 = Search.search(q, m2);
+		while ((res = r2.poll()) != null)
+			System.out.println(res.toString());
+		
+		System.out.println("#######################");
+		
+		System.out.println("proximity");
+		PriorityQueue<SearchResult> r3 = Search.search(q, m3);
+		while ((res = r3.poll()) != null)
 			System.out.println(res.toString());
 	}
 }
